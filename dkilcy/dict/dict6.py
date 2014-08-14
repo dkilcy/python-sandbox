4ec41336fd70a0d2c6da771f12fb4172cf1b3f5e
dkilcy <david@kilcyconsulting.com>
2014-08-14 13:50:56 -0400

    checkpoint

--- dkilcy/dict/dict6.py
+++ dkilcy/dict/dict6.py
@@ -15,7 +15,7 @@ def listToDict(input):
         
         node = {}
         node['id'] = id
-        for k,v in item.iteritems():
+        for k,v in item.iteritems(): 
             node[k] = v           
         
         lookup[parent_id].setdefault('children', []).append(node)
@@ -26,8 +26,9 @@ def listToDict(input):
 if __name__=='__main__':
     
     items = [
-       {'id':10, 'parent_id':None,  'a':'a10', 'b':'foo0'},
        {'id':11, 'parent_id':10,    'a':'a11', 'b':'foo1'},
+       {'id':10, 'parent_id':None,  'a':'a10', 'b':'foo0'},
+       #{'id':11, 'parent_id':10,    'a':'a11', 'b':'foo1'},
        {'id':12, 'parent_id':10,    'a':'a12', 'b':'foo2'},        
        {'id':13, 'parent_id':11,    'a':'a13', 'b':'foo3'},
        {'id':14, 'parent_id':12,    'a':'a14', 'b':'foo4'},
@@ -35,12 +36,7 @@ if __name__=='__main__':
        {'id':16, 'parent_id':None,  'a':'a16', 'b':'foo6'},   
        ]
     
-    pc = []
-    
-    for item in items: 
-        pc.append(item)
-  
-    result = listToDict(pc)
+    result = listToDict(items)
     
     print result
     print json.dumps(result)
